755081ecb7732be788b3e041a4e06700ee4109d4
Lynxtail <l.gorbachev@mail.ru>
2024-07-11 13:30:13 +0400

    finish get and post handlers

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import java.time.LocalDate;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class Controller {
+
+	private final AtomicLong counter = new AtomicLong();
+    private final LocalDate localDate = LocalDate.now();
+
+	@GetMapping("/get_request")
+	public GetResponse getRequest() {
+		return new GetResponse(counter.incrementAndGet(), "some text");
+	}
+
+    @PostMapping(value = "/post_request", consumes = "application/json", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+    public PostResponse postRequest(@RequestBody LoginForm login) {
+        return new PostResponse(counter.incrementAndGet(), login.login(), login.password(), localDate);
+    }
+    
+}
+
+// curl.exe -v -H "Accept: application/json" -H "Content-Type:application/json" -X POST --json '{\"login\":\"login\",\"password\":\"password\"}' http://localhost:8080/post_request
+// curl.exe -H "Content-Type:application/json" -X GET "http://localhost:8080/get_request"
