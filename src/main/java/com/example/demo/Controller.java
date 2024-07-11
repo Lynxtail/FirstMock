5cbf09754304c670e25d6da7f60bfe5d767c64ee
Lynxtail <l.gorbachev@mail.ru>
2024-07-11 14:00:46 +0400

    add response time

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import java.time.LocalDate;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.http.MediaType;
@@ -19,12 +20,22 @@ public class Controller {
 
 	@GetMapping("/get_request")
 	public GetResponse getRequest() {
+		try {
+			TimeUnit.SECONDS.sleep(2);
+		} catch (InterruptedException ie) {
+			Thread.currentThread().interrupt();
+		}
 		return new GetResponse(counter.incrementAndGet(), "some text");
 	}
 
     @PostMapping(value = "/post_request", consumes = "application/json", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
     public PostResponse postRequest(@RequestBody LoginForm login) {
+		try {
+			TimeUnit.SECONDS.sleep(2);
+		} catch (InterruptedException ie) {
+			Thread.currentThread().interrupt();
+		}
         return new PostResponse(counter.incrementAndGet(), login.login(), login.password(), localDate);
     }
     
