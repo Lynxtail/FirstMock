53bfb46bd01faacedac78c61b2d00f1b3df72c57
Lynxtail <l.gorbachev@mail.ru>
2024-07-16 11:04:42 +0400

    refactoring

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.controller;
 
 import java.util.concurrent.TimeUnit;
 
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.user.User;
 
 
 @RestController
--- src/main/java/com/example/demo/user/User.java
+++ src/main/java/com/example/demo/user/User.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.user;
 import java.time.LocalDate;
 
 public class User {
