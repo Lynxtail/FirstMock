2d2e549d8859d7ea6af1314962a346dc6ed5fdb4
Lynxtail <l.gorbachev@mail.ru>
2024-07-16 12:29:26 +0400

    add validation

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.http.HttpStatus;
@@ -12,7 +14,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.user.User;
-
 
 @RestController
 public class Controller {
@@ -28,20 +29,20 @@ public class Controller {
 
     @PostMapping(value = "/post_request", consumes = "application/json", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
-    public ResponseEntity<?> postRequest(@RequestBody User user) {
-		System.out.println(String.format("\n\n\n%s\n%s\n%s\n\n\n", user.login, user.password, user.date));
+    public ResponseEntity<?> postRequest(@RequestBody Map<String,String> user) {
+		Set<String> fields = Set.of("login", "password");
+		if (!(user.keySet().equals(fields))){
+			return new ResponseEntity<HttpStatus>(HttpStatus.BAD_REQUEST);
+		}
+		else if (user.get("login").isEmpty() || user.get("password").isEmpty()){
+			return new ResponseEntity<HttpStatus>(HttpStatus.BAD_REQUEST);
+		}
 		try {
 			TimeUnit.SECONDS.sleep(2);
 		} catch (InterruptedException ie) {
 			Thread.currentThread().interrupt();
 		}
-		if (user.login == null || user.login.length() == 0){
-			return new ResponseEntity<HttpStatus>(HttpStatus.BAD_REQUEST);
-		}
-		if (user.password == null || user.password.length() == 0){
-			return new ResponseEntity<HttpStatus>(HttpStatus.BAD_REQUEST);
-		}
-        return new ResponseEntity<User>(user, HttpStatus.OK);
+        return new ResponseEntity<User>(new User(user.get("login"), user.get("password")), HttpStatus.OK);
     }
     
 }
